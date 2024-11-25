7fb9d6ebf47bc0e8f9ade90f60526765f67618ec
ValyaLivsh <145009651+ValyaLivsh@users.noreply.github.com>
2024-11-25 22:53:36 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WatermarkApp
+{
+    partial class AddWatermarkApp
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnAddWatermark = new System.Windows.Forms.Button();
+            this.btnSaveImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(418, 427);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(519, 12);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(194, 46);
+            this.btnLoadImage.TabIndex = 1;
+            this.btnLoadImage.Text = "Load Image";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // btnAddWatermark
+            // 
+            this.btnAddWatermark.Location = new System.Drawing.Point(519, 74);
+            this.btnAddWatermark.Name = "btnAddWatermark";
+            this.btnAddWatermark.Size = new System.Drawing.Size(194, 44);
+            this.btnAddWatermark.TabIndex = 2;
+            this.btnAddWatermark.Text = "Add Watermark";
+            this.btnAddWatermark.UseVisualStyleBackColor = true;
+            this.btnAddWatermark.Click += new System.EventHandler(this.btnAddWatermark_Click);
+            // 
+            // btnSaveImage
+            // 
+            this.btnSaveImage.Location = new System.Drawing.Point(519, 299);
+            this.btnSaveImage.Name = "btnSaveImage";
+            this.btnSaveImage.Size = new System.Drawing.Size(194, 90);
+            this.btnSaveImage.TabIndex = 3;
+            this.btnSaveImage.Text = "Save Image";
+            this.btnSaveImage.UseVisualStyleBackColor = true;
+            this.btnSaveImage.Click += new System.EventHandler(this.btnSaveImage_Click);
+            // 
+            // AddWatermarkApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaveImage);
+            this.Controls.Add(this.btnAddWatermark);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "AddWatermarkApp";
+            this.Text = "Add Watermark App";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button btnAddWatermark;
+        private System.Windows.Forms.Button btnSaveImage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WatermarkApp
+{
+    public partial class AddWatermarkApp : Form
+    {
+        private Bitmap originalImage;
+        public AddWatermarkApp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Images|*.jpg; *.png; *.jpeg; *.bmp";
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    originalImage = new Bitmap(openFileDialog.FileName);
+                    pictureBox1.Image = originalImage;
+                }
+            }
+        }
+
+        private void btnAddWatermark_Click(object sender, EventArgs e)
+        {
+            if(originalImage == null)
+            {
+                MessageBox.Show("Please load image again");
+                return;
+            }
+            using(Graphics g = Graphics.FromImage(originalImage))
+            {
+                string watermarkText = "Watermark";
+                Font font = new Font("Arial", 220, FontStyle.Bold, GraphicsUnit.Pixel);
+                Color color = Color.FromArgb(128, 255, 255, 255);
+                SolidBrush brush = new SolidBrush(color);
+                SizeF textsize = g.MeasureString(watermarkText, font);
+                Point position = new Point(originalImage.Width - (int)textsize.Width - 10, originalImage.Height - (int)textsize.Height - 10);
+                g.DrawString(watermarkText, font, brush, position);
+            }
+            pictureBox1.Image = originalImage;
+        }
+
+        private void btnSaveImage_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null) { MessageBox.Show("No image to save"); return; }
+            using(SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "PNG|*.png|JPEG|*.jpg|BMP|*.bmp";
+                if(saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    originalImage.Save(saveFileDialog.FileName);
+                    MessageBox.Show("image is saved");
+                }
+            }
+        }
+    }
+}
